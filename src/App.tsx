320dbab5b99236b4d60a5a503cb9d3ce184db4dc
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-17 16:29:50 +0200

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import "./App.css";
 import Header from "./components/Header"; // assuming it's in the same directory
 import Post from "./components/Post";
+import TagButton from "./components/TagButton";
 
 function App() {
   return (
     <div className="App">
       <Header title="El-SooN.." />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <div className="container">
+        <div className="posts-container">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+        <div className="tags">
+          <TagButton />
+          <TagButton />
+          <TagButton />
+          <TagButton />
+          <TagButton />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TagButton.tsx
@@ -0,0 +1,11 @@
+import "./styles/TagButton.css";
+
+function TagButton() {
+  return (
+    <div className="tag-button">
+        <button className="tag-button__button">Tag Button</button>
+    </div>
+  );
+}
+
+export default TagButton;
