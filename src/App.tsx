63f66bb146540adeab521bab4b8695b98b5826b1
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-21 14:57:55 +0200

    init article component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import "./App.css";
 import Header from "./components/Header"; // assuming it's in the same directory
-import Post from "./components/Post";
-import TagButton from "./components/TagButton";
 
 function App() {
   return (
     <div className="App">
       <Header title="El-SooN.." />
       <div className="container">
-        <div className="posts-container">
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </div>
-        <div className="tags">
-          <TagButton />
-          <TagButton />
-          <TagButton />
-          <TagButton />
-          <TagButton />
-        </div>
+   
       </div>
     </div>
   );
---
+++ src/components/Article.tsx
@@ -0,0 +1,12 @@
+import exp from "constants";
+
+function Article() {
+  return (
+    <div>
+      <h1>Article</h1>
+      <p>This is an article component.</p>
+    </div>
+  );
+}
+
+export default Article;
