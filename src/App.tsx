a54bc1b896519ae8c74ee62461cf0b4a463b1dc0
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-21 15:57:12 +0200

    display json dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
 import "./App.css";
-import Header from "./components/Header"; // assuming it's in the same directory
+import Header from "./components/Header";
 import Article from "./components/Article";
+
+const articles = [
+  {
+    title: "Full-Stack Developer",
+    name: "El-SooN",
+    email: "Soon@gmail.com",
+    year: 1995,
+    content: "I love React",
+  },
+  {
+    title: "Back-End Developer",
+    name: "Yasin",
+    email: "yasin@gmail.com",
+    year: 1996,
+    content: "I love spring boot and Java",
+  },
+  {
+    title: "Front-End Developer",
+    name: "Thomas",
+    email: "Thomas@gmail.com",
+    year: 1995,
+    content: "I love JavaScript",
+  },
+  {
+    title: "Not a Developer",
+    name: "Yoshi",
+    email: "Yoshi@gmail.com",
+    year: 1995,
+    content: "I love JavaScript",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <Header title="El-SooN.." />
       <div className="container">
-        <Article
-          title="Back-end Developer"
-          name="Yasin"
-          email="Yasin@gmail.com"
-          year={1996}
-          content="I love Java"
-        />
-        <Article
-          title="Front-End Developer"
-          name="Thomas"
-          email="Thomas@gmail.com"
-          year={1995}
-          content="I love Javascrip"
-        />
-        <Article
-          title="Not a Developer"
-          name="Yoshi"
-          email="Yoshi@gmail.com"
-          year={1995}
-        />
+        {articles.map((article, index) => (
+          <Article
+            key={index}
+            title={article.title}
+            name={article.name}
+            email={article.email}
+            year={article.year}
+            content={article.content}
+          />
+        ))}
       </div>
     </div>
   );
