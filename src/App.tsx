1fffd35d9050864d798deb45b8bc7db1e84d3aba
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-21 15:21:18 +0200

    style th article

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import "./App.css";
 import Header from "./components/Header"; // assuming it's in the same directory
-
+import Article from "./components/Article";
 function App() {
   return (
     <div className="App">
       <Header title="El-SooN.." />
       <div className="container">
-   
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+          <Article />
       </div>
     </div>
   );
--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,11 +1,25 @@
-import exp from "constants";
-
+import "./styles/Article.css";
 function Article() {
   return (
-    <div>
-      <h1>Article</h1>
-      <p>This is an article component.</p>
-    </div>
+    <>
+      <div
+        className="article"
+        style={{
+          backgroundColor: "gray",
+          boxShadow: "0px 0px 10px 0px black",
+        }}
+      >
+        <h1>Article</h1>  
+        <h3>Yasin</h3>
+        <h3>Yasin@gmail.com</h3>
+        <h3>1996</h3>
+        <hr />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
+          pariatur qui doloribus animi.
+        </p>
+      </div>
+    </>
   );
 }
 
