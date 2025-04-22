fcf67e93af278eb4073931b5a30f3f7958af32db
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-22 18:03:59 +0200

    Rendering the arricle

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,4 @@
-import { title } from "process";
+import { title } from "process"; // <- this import is probably unnecessary
 import "./styles/Article.css";
 
 type ArticleProps = {
@@ -10,23 +10,36 @@ type ArticleProps = {
 };
 
 function Article(props: ArticleProps) {
-  const { title, name, email, year, content="No Content" } = props;
+  const { title, name, email, year, content = "No Content" } = props;
+
   return (
     <>
-      <div
-        className="article"
-        style={{
-          backgroundColor: "gray",
-          boxShadow: "0px 0px 10px 0px black",
-        }}
-      >
-        <h1>{title}</h1>
-        <h3>{name}</h3>
-        <h3>{email}</h3>
-        <h3>{year}</h3>
-        <hr />
-        <p>{content}</p>
-      </div>
+      {content == null || content === "No Content" ? (
+        <div
+          style={{
+            backgroundColor: "darkgray",
+            boxShadow: "0px 0px 10px 0px black",
+          }}
+          className="article no-content"
+        >
+          No Content
+        </div>
+      ) : (
+        <div
+          className="article"
+          style={{
+            backgroundColor: "gray",
+            boxShadow: "0px 0px 10px 0px black",
+          }}
+        >
+          <h1>{title}</h1>
+          <h3>{name}</h3>
+          <h3>{email}</h3>
+          <h3>{year}</h3>
+          <hr />
+          <p>{content}</p>
+        </div>
+      )}
     </>
   );
 }
