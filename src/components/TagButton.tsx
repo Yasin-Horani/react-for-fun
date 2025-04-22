bc414c1f6778d22edd9d23499c7d2a168719b223
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-22 16:53:02 +0200

    make style for one button with using props

--- src/components/TagButton.tsx
+++ src/components/TagButton.tsx
@@ -1,9 +1,17 @@
 import "./styles/TagButton.css";
 
-function TagButton() {
+type TagButtonProps = { 
+  tagName: string;
+  image?: string;
+  className?: string;
+}
+
+function TagButton(props: TagButtonProps) {
+  const { tagName, image, className } = props;
   return (
     <div className="tag-button">
-        <button className="tag-button__button">Tag Button</button>
+        <button className="tag-button">{tagName}</button>
+        <img className={className} src={image} alt="" />
     </div>
   );
 }
