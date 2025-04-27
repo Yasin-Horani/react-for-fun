665be96835f5ae7c0e0da0abbd718df015f04ddd
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-27 17:35:39 +0200

    List Rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Article from "./components/Article";
 import TagButton from "./components/TagButton";
+import { idText } from "typescript";
 
 const articles = [
   {
@@ -33,6 +34,35 @@ const articles = [
   },
 ];
 
+const buttons = [
+  { 
+    id: 1,
+    tagName: "button 1",
+  },
+  {
+    id: 2,
+    tagName: "button 2",
+  },
+  {
+    id: 3,
+    tagName: "button 3",
+  },
+  {
+    id: 4,
+    tagName: "button 4",
+  },
+  {
+    id: 5,
+    tagName: "syrian flag",
+    className: "syrianFlag",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fflagmakers.co.uk%2Fmedia%2Fg0ddkbjp%2F1932-1958_1961-1963.png%3Fanchor%3Dcenter%26mode%3Dcrop%26width%3D1530%26height%3D0%26rnd%3D132439467255100000&f=1&nofb=1&ipt=4f4304b985dc1fe9d6d1e6a6974db437af3eddbec1459153b6b60fff129c4c58",
+  },
+  {
+    id: 6,
+    tagName: "button 6",
+  },
+];
+
 function App() {
   return (
     <div className="App">
@@ -51,16 +81,15 @@ function App() {
           ))}
         </div>
         <div className="buttons">
-          <TagButton tagName="button 1" />
-          <TagButton tagName="button 2" />
-          <TagButton tagName="button 3" />
-          <TagButton tagName="button 4" />
-          <TagButton
-            tagName="button 5"
-            className="syrianFlag"
-            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fflagmakers.co.uk%2Fmedia%2Fg0ddkbjp%2F1932-1958_1961-1963.png%3Fanchor%3Dcenter%26mode%3Dcrop%26width%3D1530%26height%3D0%26rnd%3D132439467255100000&f=1&nofb=1&ipt=4f4304b985dc1fe9d6d1e6a6974db437af3eddbec1459153b6b60fff129c4c58"
-          />
-          <TagButton tagName="button 6" />
+          {buttons.map((button) => (
+            <TagButton
+              key={button.id}
+              tagName={button.tagName}
+              className={button.className}
+              image={button.image}
+            />
+          ))}
+
         </div>
       </div>
     </div>
