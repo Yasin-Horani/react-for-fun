0170f7a9629a0f7a9b7b188f7b2664cac1fac4bb
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-28 17:09:08 +0200

    state with forms

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,29 +1,58 @@
+import { useState } from "react";
+
 function MyForm() {
-    return (
-      <form
-        style={{
-          border: "2px solid red",
-          marginTop: "20px",
-          padding: "20px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "10px" 
-        }}
-      >
-        <label style={{ display: "flex", flexDirection: "column" }}>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label style={{ display: "flex", flexDirection: "column" }}>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <button style={{ width: "100px", marginTop: "10px" }} type="submit">
-          Submit
-        </button>
-      </form>
-    );
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   }
-  
-  export default MyForm;
-  
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(formData);
+  }
+
+  return (
+    <form
+      style={{
+        border: "2px solid red",
+        marginTop: "20px",
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <label style={{ display: "flex", flexDirection: "column" }}>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label style={{ display: "flex", flexDirection: "column" }}>
+        Email:
+        <input
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <button style={{ width: "100px", marginTop: "10px" }} type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default MyForm;
