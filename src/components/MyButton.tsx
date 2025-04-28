0269f47495e5a8f288942aff20d5820a4f184865
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-28 15:23:38 +0200

    update myButton

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 
 function MyButton() {
-  const [name, setName] = useState("Justin");
-
+  const [name, setName] = useState("El-SooN..");
   function handleClick() {
-    setName("Yasin"); 
-    if (name === "Yasin") {
-      setName("Justin");
+    setName("El-SooN.. is the best!");
+    if (name === "El-SooN.. is the best!") {
+      setName("El-SooN..");
     }
   }
-
   return (
     <>
       <button onClick={handleClick}>Click me!</button>
