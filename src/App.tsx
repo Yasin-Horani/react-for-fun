fc9f9fc49c027d401f3229d6a461233f4a8ef252
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-04-29 19:25:38 +0200

    remove button 6

--- src/App.tsx
+++ src/App.tsx
@@ -60,10 +60,6 @@ const buttons = [
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fflagmakers.co.uk%2Fmedia%2Fg0ddkbjp%2F1932-1958_1961-1963.png%3Fanchor%3Dcenter%26mode%3Dcrop%26width%3D1530%26height%3D0%26rnd%3D132439467255100000&f=1&nofb=1&ipt=4f4304b985dc1fe9d6d1e6a6974db437af3eddbec1459153b6b60fff129c4c58",
   },
-  {
-    id: 6,
-    tagName: "button 6",
-  },
 ];
 
 function App() {
